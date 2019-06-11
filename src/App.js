a78848bdc04254ba8055adce9856e935f659746c
Sarah Nouh <sarah.nouh17@gmail.com>
2019-06-12 01:53:43 +0200

    Video Editing Interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @Author: sarahnouh
+ * @Date:   1985-10-26T10:15:00+02:00
+ * @Last modified by:   sarahnouh
+ * @Last modified time: 2019-06-12T00:19:40+02:00
+ */
+
+import React from "react";
+import videoImg from "./images/man.jpg";
+import rec_icon from "./images/rec_icon.png";
+import start_rec from "./images/start_rec.png";
+import thumbnail from "./images/thumbnail.png";
+import focus from "./images/focus.png";
+import frame from "./images/frame.png";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row">
+      <div className="col-12 darkblue-header" />
+      <div className="col-3 orange-sidebar" />
+      <div className="col-12 col-md-9 video-content text-center">
+        <div className="row">
+          <h3 className="col-12 text-center video-title">The Video Title</h3>
+          <div className="col-12">
+            <div className="video-image-container">
+              <img className="video-img" src={videoImg} alt="" />
+              {/* <img className="frame" src={frame} alt="" /> */}
+              <img className="start-rec" src={start_rec} alt="" />
+              <img className="focus" src={focus} alt="" />
+              <div className="rec-items">
+                <img className="rec-icon" src={rec_icon} alt="" />
+                <p className="rec-text">rec</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="video-img" /> */}
+        <div className="row">
+          <div className="offset-md-3 col-4 col-md-2 thumbnail">
+            <img src={thumbnail} className=" thumbnail-img" />
+            <div className="overlay" />
+          </div>
+          <div className="col-4 col-md-2 thumbnail">
+            <img src={thumbnail} className=" thumbnail-img" />
+            <div className="overlay" />
+          </div>
+          <div className="col-4 col-md-2 thumbnail">
+            <img src={thumbnail} className=" thumbnail-img" />
+            <div className="overlay" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+ * @Author: sarahnouh
+ * @Date:   1985-10-26T10:15:00+02:00
+ * @Last modified by:   sarahnouh
+ * @Last modified time: 2019-06-12T00:32:01+02:00
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
