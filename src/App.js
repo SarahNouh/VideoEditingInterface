a1a9ecd4052013e544e0b5f8660d359b5f3a004e
Sarah Nouh <sarah.nouh17@gmail.com>
2019-06-12 02:20:24 +0200

    Adding in some responsive changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
  * @Author: sarahnouh
  * @Date:   1985-10-26T10:15:00+02:00
  * @Last modified by:   sarahnouh
- * @Last modified time: 2019-06-12T00:19:40+02:00
+ * @Last modified time: 2019-06-12T02:14:51+02:00
  */
 
 import React from "react";
@@ -16,39 +16,38 @@ import "./App.scss";
 
 function App() {
   return (
-    <div className="row">
-      <div className="col-12 darkblue-header" />
-      <div className="col-3 orange-sidebar" />
-      <div className="col-12 col-md-9 video-content text-center">
-        <div className="row">
-          <h3 className="col-12 text-center video-title">The Video Title</h3>
-          <div className="col-12">
-            <div className="video-image-container">
-              <img className="video-img" src={videoImg} alt="" />
-              {/* <img className="frame" src={frame} alt="" /> */}
-              <img className="start-rec" src={start_rec} alt="" />
-              <img className="focus" src={focus} alt="" />
-              <div className="rec-items">
-                <img className="rec-icon" src={rec_icon} alt="" />
-                <p className="rec-text">rec</p>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12 darkblue-header" />
+        <div className="col-3 orange-sidebar" />
+        <div className="col-12 col-md-9 video-content text-center">
+          <div className="row">
+            <h3 className="col-12 text-center video-title">The Video Title</h3>
+            <div className="col-12">
+              <div className="video-image-container">
+                <img className="video-img" src={videoImg} alt="" />
+                <img className="start-rec" src={start_rec} alt="" />
+                <img className="focus" src={focus} alt="" />
+                <div className="rec-items">
+                  <img className="rec-icon" src={rec_icon} alt="" />
+                  <p className="rec-text">rec</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-
-        {/* <div className="video-img" /> */}
-        <div className="row">
-          <div className="offset-md-3 col-4 col-md-2 thumbnail">
-            <img src={thumbnail} className=" thumbnail-img" />
-            <div className="overlay" />
-          </div>
-          <div className="col-4 col-md-2 thumbnail">
-            <img src={thumbnail} className=" thumbnail-img" />
-            <div className="overlay" />
-          </div>
-          <div className="col-4 col-md-2 thumbnail">
-            <img src={thumbnail} className=" thumbnail-img" />
-            <div className="overlay" />
+          <div className="row">
+            <div className="offset-md-3 col-4 col-md-2 thumbnail">
+              <img src={thumbnail} className=" thumbnail-img" />
+              <div className="overlay" />
+            </div>
+            <div className="col-4 col-md-2 thumbnail">
+              <img src={thumbnail} className=" thumbnail-img" />
+              <div className="overlay" />
+            </div>
+            <div className="col-4 col-md-2 thumbnail">
+              <img src={thumbnail} className=" thumbnail-img" />
+              <div className="overlay" />
+            </div>
           </div>
         </div>
       </div>
